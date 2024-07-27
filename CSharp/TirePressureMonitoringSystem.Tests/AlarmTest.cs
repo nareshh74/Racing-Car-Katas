1b3a204af97d7c4b56fbc3b1412e24bf0289e3fe
Naresh Kumar <kumarnaresh@microsoft.com>
2024-07-27 15:50:42 +0530

    nit - move class

--- CSharp/TirePressureMonitoringSystem.Tests/AlarmTest.cs
+++ CSharp/TirePressureMonitoringSystem.Tests/AlarmTest.cs
@@ -15,55 +15,55 @@ namespace TDDMicroExercises.TirePressureMonitoringSystem
                 Assert.Same(sensor, alarm.Sensor);
             }
         }
-    }
 
-    public class CheckShould
-    {
-        [Fact]
-        public void Get_value_from_sensor()
+        public class CheckShould
         {
-            // Arrange
-            var sensorMock = new Mock<ISensor>();
-            Alarm alarm = new(sensorMock.Object);
+            [Fact]
+            public void Get_value_from_sensor()
+            {
+                // Arrange
+                var sensorMock = new Mock<ISensor>();
+                Alarm alarm = new(sensorMock.Object);
 
-            // Act
-            alarm.Check();
+                // Act
+                alarm.Check();
 
-            // Assert
-            sensorMock.Verify(x => x.PopNextPressurePsiValue(), Times.Once);
-        }
+                // Assert
+                sensorMock.Verify(x => x.PopNextPressurePsiValue(), Times.Once);
+            }
 
-        [Theory]
-        [InlineData(Alarm.LowPressureThreshold - 1, true)]
-        [InlineData(Alarm.LowPressureThreshold, false)]
-        [InlineData(Alarm.HighPressureThreshold + 1, true)]
-        public void Set_alarm_as_expected(int value, bool expected)
-        {
-            // Arrange
-            var sensorMock = new Mock<ISensor>();
-            sensorMock.Setup(x => x.PopNextPressurePsiValue()).Returns(value);
-            Alarm alarm = new(sensorMock.Object);
+            [Theory]
+            [InlineData(Alarm.LowPressureThreshold - 1, true)]
+            [InlineData(Alarm.LowPressureThreshold, false)]
+            [InlineData(Alarm.HighPressureThreshold + 1, true)]
+            public void Set_alarm_as_expected(int value, bool expected)
+            {
+                // Arrange
+                var sensorMock = new Mock<ISensor>();
+                sensorMock.Setup(x => x.PopNextPressurePsiValue()).Returns(value);
+                Alarm alarm = new(sensorMock.Object);
 
-            // Act
-            alarm.Check();
+                // Act
+                alarm.Check();
 
-            // Assert
-            Assert.Equal(expected, alarm.AlarmOn);
-        }
+                // Assert
+                Assert.Equal(expected, alarm.AlarmOn);
+            }
 
-        [Fact]
-        public void Increment_alarm_count()
-        {
-            // Arrange
-            var sensorMock = new Mock<ISensor>();
-            sensorMock.Setup(x => x.PopNextPressurePsiValue()).Returns(Alarm.LowPressureThreshold - 1);
-            Alarm alarm = new(sensorMock.Object);
+            [Fact]
+            public void Increment_alarm_count()
+            {
+                // Arrange
+                var sensorMock = new Mock<ISensor>();
+                sensorMock.Setup(x => x.PopNextPressurePsiValue()).Returns(Alarm.LowPressureThreshold - 1);
+                Alarm alarm = new(sensorMock.Object);
 
-            // Act
-            alarm.Check();
+                // Act
+                alarm.Check();
 
-            // Assert
-            Assert.Equal(1, alarm.AlarmCount);
+                // Assert
+                Assert.Equal(1, alarm.AlarmCount);
+            }
         }
     }
 }
