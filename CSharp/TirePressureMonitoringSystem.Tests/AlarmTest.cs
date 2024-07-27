3ee29b043ede4bedd70805ad273a6bfec5e76cd9
Naresh Kumar <kumarnaresh@microsoft.com>
2024-07-27 15:53:52 +0530

    get api tests

--- CSharp/TirePressureMonitoringSystem.Tests/AlarmTest.cs
+++ CSharp/TirePressureMonitoringSystem.Tests/AlarmTest.cs
@@ -65,5 +65,26 @@ namespace TDDMicroExercises.TirePressureMonitoringSystem
                 Assert.Equal(1, alarm.AlarmCount);
             }
         }
+
+        public class AlarmOnShould
+        {
+            [Theory]
+            [InlineData(Alarm.LowPressureThreshold - 1, true)]
+            [InlineData(Alarm.LowPressureThreshold, false)]
+            [InlineData(Alarm.HighPressureThreshold + 1, true)]
+            public void Get_alarm_on_as_expected(int value, bool expected)
+            {
+                // Arrange
+                var sensorMock = new Mock<ISensor>();
+                sensorMock.Setup(x => x.PopNextPressurePsiValue()).Returns(value);
+                Alarm alarm = new(sensorMock.Object);
+
+                // Act
+                alarm.Check();
+
+                // Assert
+                Assert.Equal(expected, alarm.AlarmOn);
+            }
+        }
     }
 }
